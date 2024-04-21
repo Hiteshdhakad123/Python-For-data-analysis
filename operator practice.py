90a50135c42b09f83526f569e3b325363c0f2b0b
Hiteshdhakad123 <113380556+Hiteshdhakad123@users.noreply.github.com>
2024-04-21 23:39:16 +0530

    operator in python

---
+++ operator practice.py
@@ -0,0 +1,57 @@
+# types of operator
+# 1.Arithmatic operator example:
+
+a=2
+b=5
+c=a+b
+print(c)
+
+a=2
+b=5
+c=a-b
+print(c)
+
+a=2
+b=5
+c=a*b
+print(c)
+
+a=2
+b=5
+c=a/b
+print(c)
+
+a=2
+b=5
+c=a//b
+print(c)
+
+a=2
+b=5
+c=a%b
+print(c)
+
+a=2
+b=5
+c=a**b
+print(c)
+
+# assignment operator example
+# =,+=,-=,*=,/=,//=,%=,**=
+
+x=5
+y=3
+print(x=y)
+
+
+
+
+
+
+
+
+
+
+
+
+
