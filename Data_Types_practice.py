f194dfc43cd752cdb7e936d86c3a9b8a8519770e
Hiteshdhakad123 <113380556+Hiteshdhakad123@users.noreply.github.com>
2024-04-21 23:36:49 +0530

    data type in python

---
+++ Data_Types_practice.py
@@ -0,0 +1,23 @@
+# Data types
+# example of  type data types
+
+x=10
+print(type(x))
+
+x=10.0
+print(type(x))
+
+x='hello'
+print(type(x))
+
+x=True
+print(type(x))
+
+x=10+2j
+print(type(x))
+
+AGE=20
+if AGE>18:
+ print('eligible')
+ 
+print('not eligible')
