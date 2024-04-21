99a4037a021ba32e8c0753f7509ee2ab5f37bb16
Hiteshdhakad123 <113380556+Hiteshdhakad123@users.noreply.github.com>
2024-04-21 23:40:23 +0530

    casting/conversion in python

---
+++ casting conversion practice.py
@@ -0,0 +1,105 @@
+# types of casting
+# example of casting/conversion
+
+# int to float
+x=5
+y=float(x)
+print(type(y))
+
+# int to str
+x=5
+y=str(x)
+print(type(y))
+
+# int to bool
+x=5
+y=bool(x)
+print(y)
+
+# int to complex
+x=5
+y=complex(x)
+print(y)
+
+# float to int
+x=2.0
+y=int(x)
+print(y)
+
+# float to str
+x=2.0
+y=str(x)
+print(y)
+
+# float to bool
+x=2.0
+y=bool(x)
+print(y)
+
+# float to complex
+x=2.0
+y=complex(x)
+print(y)
+
+# str to int
+x=10
+y=str(x)
+print(y)
+
+# str to float
+x=10
+y=float(x)
+print(y)
+
+# str to bool
+x=10
+y=bool(x)
+print(y)
+
+# str to complex
+x=10
+y=complex(x)
+print(y)
+
+
+# bool to int
+x=True
+y=int(x)
+print(y)
+
+
+# bool to float
+x=False
+y=float(x)
+print(y)
+
+# bool to str
+x=True
+y=str(x)
+print(y)
+
+# bool to complex
+x=True
+y=complex(x)
+print(y)
+
+# complex to str
+x=2+5j
+y=str(x)
+print(y)
+
+# complex to bool
+x=2+5j
+y=bool(x)
+print(y)
+
+
+
+
+
+
+
+
+
+
+
