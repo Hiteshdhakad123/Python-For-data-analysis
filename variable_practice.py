cfff939a61ece4974a2993d44b7e9817e97f4ad0
Hiteshdhakad123 <113380556+Hiteshdhakad123@users.noreply.github.com>
2024-04-21 23:35:03 +0530

    variable in python

---
+++ variable_practice.py
@@ -0,0 +1,33 @@
+# variable example
+x=5
+print(x)
+
+x=5
+y="hitesh"
+print(x)
+print(y)
+
+a=4
+A="sani"
+print(a)
+print((A))
+
+# variable type()
+x=10
+print(type(x))
+
+x=10.0
+print(type(x))
+
+x='10'
+print(type(x))
+
+x=True
+print(type(x))
+
+x=10+2j
+print(type(x))
+
+
+
+
