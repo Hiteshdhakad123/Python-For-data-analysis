91725a1db4629816bf50a540c00669a81e7e3e2e
Hiteshdhakad123 <113380556+Hiteshdhakad123@users.noreply.github.com>
2024-04-21 23:41:34 +0530

    slicing in string python

---
+++ slicing in string.py
@@ -0,0 +1,58 @@
+# string exmple 
+    #-5  -4 -3 -2 -1 (-ve)
+    # 0  1  2  3  4  (+ve)
+# x={10,20,30,40,50} index value
+#   x=10=0
+#   x=20=1
+#   x=30=2
+#   x=40=3
+#   x=50=4
+
+# string follow index rule:
+# ex-1 
+    # x='abc'
+    #   a=0
+    #   b=1
+    #   c=2
+
+# ex-2
+    # x='abcd'
+    # x[0]=a
+    # x[d]=-d
+
+# how to acess single element in string:
+# x=hello world!?
+# Q,L
+# x=[2]
+
+# slicing in string example
+
+# 1.normal slicing:-
+# 2.pattern slicing:-
+
+# syntax normal slicing
+# note- index 1 value more range (+0,-1)
+# {variablename[startingindexvalue:range]}
+# ex-
+#   x= hello world!?
+#   Q.FIND lo
+#    x=[3:5] (1+ add)
+
+#   Q. world?
+#      x=[5:10]
+
+# to extract all character and symbol sepretely
+# x= hello world!?
+# x=[0:10]
+# x=[10:12]
+
+slicing in (-ve)
+
+# x='abcdefghijkl'
+# Q.ef(-ve)
+#   x=[-8:-6]
+ 
+
+     
+x='abcdefghijkl'
+print[-4:-2]
