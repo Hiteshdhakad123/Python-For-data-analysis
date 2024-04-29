c01dca9c1de69c2f8b47de4ef03e5e9ed09c2ece
Hiteshdhakad123 <113380556+Hiteshdhakad123@users.noreply.github.com>
2024-04-29 12:44:09 +0530

    while loop example

---
+++ for_while_Loop.py
@@ -0,0 +1,26 @@
+#  while loop example :
+# 1 to 100 even no
+
+# x=2
+# while x<100:
+#   print(x)
+#   x=x+2
+
+
+# #   1 to 100 odd no
+# i=1
+# while i<100:
+#   print(i)
+#   i+=2
+
+# 100 to 1 even no reverse 
+# i=100
+# while i>2:
+#     print(i)
+#     i=i-2
+
+# 100 to 1 odd no reverse
+i=101
+while i>2:
+    print(i)
+    i=i-1
