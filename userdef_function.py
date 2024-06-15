76890d4d768d018aef03aa86dbd67a8200e8ee32
Hiteshdhakad123 <113380556+Hiteshdhakad123@users.noreply.github.com>
2024-06-15 11:49:27 +0530

    userdef fun print and return

---
+++ userdef_function.py
@@ -0,0 +1,34 @@
+#user define function:
+#syntax: 
+#    def function():
+#        --------code
+ 
+#behaviour function/method: example
+#type :print
+
+# def greet():    
+#     print("hello")
+# greet()
+
+#type:return
+
+# def greeting():
+#     return("mahesh")
+# y=greeting()              
+# print(y)
+ 
+#2.
+# def greeting(): 
+#     return("hitesh")
+# print(greeting()) #second example direct print use
+
+
+#types of parameter and arguments:-
+# variable = parameter
+# value =  argument
+
+#example:
+def square(x):
+    return x*x
+result=square(5)
+print(result)
