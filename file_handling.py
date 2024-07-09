c2b56a639ce829dc084e94a374a0defb5291f8fb
Hiteshdhakad123 <113380556+Hiteshdhakad123@users.noreply.github.com>
2024-07-09 17:10:01 +0530

    file handling,read ,write,create,remove

---
+++ file_handling.py
@@ -0,0 +1,18 @@
+#file handling
+
+#create table
+#f=open("demo.txt","x")  # x means create new table
+
+#write text in demo.txt  file
+# f=open("demo.txt","wt")
+# f.write("this is used to file handling")
+
+# #read file text name
+# f=open("demo.txt","rt")
+# print(f.read())
+#print(f.readline()) # used to unlimited element
+#print(f.readline())
+
+#remove file:
+import os
+os.remove("demo.txt")
